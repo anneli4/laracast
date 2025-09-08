56f35eedca0fc5127b02b0823482c11409089da4
Anneli Peep <anneli.peep@gmail.com>
2025-09-08 16:33:58 +0300

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test User',
+            'last_name' => 'Example',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            JobSeeder::class,
+        ]);
+
     }
 }
