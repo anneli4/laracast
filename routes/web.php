9eca2c39ac5ae8b92f588799b3ce0373fcd2a11f
Anneli Peep <anneli.peep@gmail.com>
2025-09-10 16:04:22 +0300

    Validate

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,38 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function ()  {
-    $job = Job::with('employer')->paginate(3);
-return view('jobs' , [
+    $job = Job::with('employer')->latest()->paginate(3);
+return view('jobs.index' , [
         'jobs' =>  $job
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
+
+Route::post('/jobs', function () {
+
+    request()->validate([
+        'title' => 'required',
+        'salary' => 'required|numeric'
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1 // Assuming the employer ID is 1 for this example
+    ]);
+
+    return redirect('/jobs');
+});
+
+
 
 Route::get('/contact', function () {
     return view('contact');
