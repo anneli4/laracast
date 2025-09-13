e4fac11f2298553666e4243a4d4ae8bb64afe6dc
Anneli Peep <anneli.peep@gmail.com>
2025-09-13 13:43:34 +0300

    Login and Registration

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterUserController extends Controller
 {
@@ -12,6 +15,18 @@ class RegisterUserController extends Controller
     }
     public function store()
     {
+        $attributes = request()->validate([
+            'first_name'        => ['required'],
+            'last_name'         => ['required'],
+            'email'             => ['required', 'email'],
+            'password'          => ['required',Password::min(6), 'confirmed'],
 
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -13,6 +14,22 @@ class SessionController extends Controller
 
     public function store()
     {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
+        if (!Auth::attempt($attributes));
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+        request()->session()->regenerate();
+        return redirect('/jobs');
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
